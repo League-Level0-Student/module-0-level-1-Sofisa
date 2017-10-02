6a06cf78d5270a9fe9cabf882726eaaa0c01393d
Sofisa <league@WorthyMini-6.attlocal.net>
2017-10-01 17:32:04 -0700

    Starshow not complete but complete x geeks

--- src/XGeeks.java
+++ src/XGeeks.java
@@ -11,15 +11,36 @@ import javax.swing.JOptionPane;
 * the superpowers of all the people in the classroom. E.g. When I type "June",
 * your program should say "June's superpower is writing recipes".
 */
+
 public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+String grace = "tennis";
+String emerson = "magical abilities";
+String sofia = "eating";
+String priyankaa = "flexibility";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
-		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
-	}
+String name = JOptionPane.showInputDialog("Whats your name?");
+		// 3. Show the superpower in a pop-up, depending on the name entered.
+if (name.equalsIgnoreCase("grace")) {
+	JOptionPane.showMessageDialog(null, "Your superpower is " + grace);
 }
+if (name.equalsIgnoreCase("emerson")) {
+	JOptionPane.showMessageDialog(null, "Your superpower is " + emerson);
+}
+if (name.equalsIgnoreCase("sofia")) {
+	JOptionPane.showMessageDialog(null, "Your superpower is " + sofia);
+}
+if (name.equalsIgnoreCase("priyankaa")) {
+	JOptionPane.showMessageDialog(null, "Your superpower is " + priyankaa);
+}
+else{
+	JOptionPane.showMessageDialog(null, "Sorry that name does not have a superpower");
+}
+}
+}
+
+
+
 
