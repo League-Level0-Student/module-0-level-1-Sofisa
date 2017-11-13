3b1ee8ab46b80d4cffe0b6789aa9f2edad4b5c36
Sofisa <league@WorthyMini-6.attlocal.net>
2017-11-12 17:32:41 -0800

    Practice

---
+++ src/Practice.java
@@ -0,0 +1,18 @@
+
+public class Practice {
+
+
+	
+public static void main(String[] args) {
+	
+	double pointsScored = 45;
+	double pointsPossible = 90;
+	double grade = pointsScored / pointsPossible;
+	System.out.println (grade);
+	String name = "Sofia";
+	char secondLetter = name.charAt(1);
+	System.out.println (secondLetter);
+	char lastLetter = name.charAt(name.length() - 1);
+	System.out.println(lastLetter);
+}
+}
